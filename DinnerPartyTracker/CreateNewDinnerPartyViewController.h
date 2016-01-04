947e7b11bcc785820e4887ac069f2e8d3cb250c3
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2016-01-04 15:27:40 -0800

    Formatted Buttons, Start of Create New Dinner Party VC

---
+++ DinnerPartyTracker/CreateNewDinnerPartyViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateNewDinnerPartyViewController.h
+//  DinnerPartyTracker
+//
+//  Created by Lindsey on 1/4/16.
+//  Copyright © 2016 Lindsey Boggio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateNewDinnerPartyViewController : UIViewController
+
+@end
---
+++ DinnerPartyTracker/CreateNewDinnerPartyViewController.m
@@ -0,0 +1,37 @@
+//
+//  CreateNewDinnerPartyViewController.m
+//  DinnerPartyTracker
+//
+//  Created by Lindsey on 1/4/16.
+//  Copyright © 2016 Lindsey Boggio. All rights reserved.
+//
+
+#import "CreateNewDinnerPartyViewController.h"
+
+@interface CreateNewDinnerPartyViewController ()
+
+@end
+
+@implementation CreateNewDinnerPartyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
