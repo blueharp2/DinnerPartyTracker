89caeead8e0c34e887db374f7c939e3ac6448dc1
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2016-03-14 15:29:38 -0700

    Start of CreateNewDinnerPartyDetailViewController

---
+++ DinnerPartyTracker/CreateNewDinnerPartyDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateNewDinnerPartyDetailViewController.h
+//  DinnerPartyTracker
+//
+//  Created by Lindsey on 3/14/16.
+//  Copyright © 2016 Lindsey Boggio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateNewDinnerPartyDetailViewController : UIViewController
+
+@end
